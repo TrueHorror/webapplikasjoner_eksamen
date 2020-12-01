acc31172d3ae70f2feafbecb1e088e8f3a50d1f7
Fredrik Olsvold <fredrik.olsvold@gmail.com>
2020-12-01 17:38:34 +0100

    Added styled components to home page

--- rorlegger/client/src/App.js
+++ rorlegger/client/src/App.js
@@ -1,27 +1,26 @@
 import './App.css';
 import Nav from './layouts/Nav'
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Offices from './pages/Offices'
-import Articles from './pages/Articles'
-import Contact from './pages/Contact'
+import { BrowserRouter as Router} from 'react-router-dom'
 import Routes from './routes/Routes'
+import Footer from './layouts/Footer'
+import {StyledHeader, StyledFooter, StyledLogo } from './styles/Styled.jsx'
+
 
 function App() {
   return (
     <div className="App">
+  
       <Router>
-        <header>
+        <StyledHeader>
+        <StyledLogo>FG</StyledLogo>
           <Nav/>
-        </header>
-        <main>
-          <Routes />
-          
-        </main>
-        <footer>
-          
-        </footer>
+        </StyledHeader>
+        <Routes />
+        <StyledFooter>
+          <Footer />
+        </StyledFooter>
         </Router>
+    
     </div>
   );
 }
---
+++ rorlegger/client/src/styles/Styled.jsx
@@ -0,0 +1,176 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+
+//Style and CSS taken from lectures and earlier mandatory exercises
+
+//header
+export const StyledHeader = styled.header`
+width: 100%;
+box-shadow: 0px 5px 5px -5px #00000029;
+display: flex;
+
+
+`;
+
+//Styled Logo
+
+export const StyledLogo = styled.h1`
+padding-left: 20px;
+
+`;
+
+//Nav in header
+export const StyledNav = styled.nav`
+ margin-left: auto;
+ margin-right: 0;
+
+
+`;
+
+export const NavMenu = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  
+`;
+
+export const NavMenuItem = styled.li`
+  padding: 0 20px;
+  display: block;
+  float: left;
+
+  &:first-child {
+    padding-left: 0;
+  }
+
+  & > a {
+    color: black;
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 3.456;
+    padding: 5px 10px;
+    text-decoration: none;
+    
+
+    &.active {
+      color: #469fb9;
+    }
+
+    &:hover {
+      border-bottom: 4px solid #469fb9;
+    }
+  }
+
+`;
+
+//Banner, Logo
+export const StyledBanner = styled.figure`
+  width: 100%;
+  margin: 0 auto;
+  background-color: #DBDBDB;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+
+
+//Main-Content with rom and column. Inspiration from: https://dev.to/drews256/ridiculously-easy-row-and-column-layouts-with-flexbox-1k01 -----------------------------------------------------------------
+export const StyledMainContent = styled.main`
+  margin: 0 auto;
+  width: 90%;
+ 
+  
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+  
+
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  flex: 1;
+  padding: 10px;
+
+ 
+`;
+
+export const DoubleColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  flex: 2;
+  padding: 10px;
+`;
+
+
+
+export const StyledMainSections = styled.section`
+  background-color: #DBDBDB;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+`;
+
+
+
+//Aside
+export const StyledAside = styled.aside`
+  background-color: #DBDBDB;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+//Main Links
+
+export const StyledLinks = styled(Link)`
+  font-size: 20px;
+  text-decoration: none;
+  color: #000;
+  &:hover {
+      border-bottom: 4px solid #469fb9;
+    }
+
+`;
+
+//------------------------------------------------------------------------------------------
+
+//footer
+
+export const StyledFooter = styled.footer`
+  width: 100%;
+  display: flex;
+  box-shadow: 0 -5px 5px -5px #00000029;
+  height: 30px;
+
+`;
+
+export const StyledFooterList = styled.ul`
+  list-style-type: none;
+  margin: 0 auto;
+  padding: 0;
+  
+`;
+
+export const StyledFooterListItems = styled.li`
+  padding: 0 20px;
+  display: block;
+  float: left;
+  justify-content: space-between;
+
+`;
+
+
