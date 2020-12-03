393442378a67323c2d67f11fe89b688863f98e4c
Trond Graabræk <43748103+trondag@users.noreply.github.com>
2020-12-03 12:04:45 +0100

    .

--- rorlegger/server/tests/users.spec.js
+++ rorlegger/server/tests/users.spec.js
@@ -1,4 +1,6 @@
-UserService = require('../services/user.service')
+const UserService = require('../services/user.service')
+const User = require('../models/user.model')
+jest.mock('../models/user.model')
 
 describe("getUserType()", () => {
   it("should return 0 for admin", () => {
@@ -9,14 +11,47 @@ describe("getUserType()", () => {
   });
 });
 
-describe("loginUser()", async () => {
+describe("loginUser()", () => {
+  User.findOne.mockResolvedValue({
+    Password: "$2b$10$kBqlrGT9SFj03HQ0XbiU/eZVwSZXCJdz6dVrs/jF6TJIYbyBlH8Ee"
+  })
+  process.env.JWT_SECRET = '777f9cf0-354a-11eb-adc1-0242ac120002'
   it ("should login admin and return a token", async () => {
     let loginDetails = {
       Email: "admin@admin.no",
       Password: "drossap"
     }
-    import
+    expect(await UserService.loginUser(loginDetails)).toMatch(/eyJ0/)
+    expect(User.findOne).toHaveBeenCalledTimes(1)
+  })
+  it ("should return null when password is wrong", async () => {
+    let loginDetails = {
+      Email: "admin@admin.no",
+      Password: "drosap"
+    }
+    expect(await UserService.loginUser(loginDetails)).toBeNull()
+  })
+  /*it ("should fail when db is down", async () => {
+    User.findOne.mockResolvedValue(undefined)
+    let loginDetails = {
+      Email: "admin@admin.no",
+      Password: "drossap"
+    }
+    expect(await UserService.loginUser(loginDetails)).toThrow('Login failed')
+  })*/
+})
 
-    expect(await UserService.loginUser(loginDetails)).toBe('hei')
+describe("createUser()", () => {
+  it ("should create a user", async () => {
+    let userDetails = {
+      "GivenName": "Ole",
+      "FamilyName": "Olsen",
+      "UserType": "admin",
+      "Email": "admin@admin.no",
+      "Password": "drossap"
+    }
+
+    expect(await UserService.createUser(userDetails)).toBeUndefined()
+    expect(User.create).toBeUndefined()
   })
 })
