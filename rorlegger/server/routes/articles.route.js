fdc3b0acbb33bc1a8581dcca0adddcda9df02486
Trond Graabræk <43748103+trondag@users.noreply.github.com>
2020-12-05 20:11:09 +0100

    .

--- rorlegger/server/routes/articles.route.js
+++ rorlegger/server/routes/articles.route.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const router = express.Router();
 
-var ArticleController = require('../controllers/article.controller')
+const ArticleController = require('../controllers/article.controller')
+const secret = process.env.JWT_SECRET
+const jwt = require('express-jwt')
 
-router.get('/', ArticleController.getArticles)
+router.get('/', ArticleController.getNonSecretArticles)
+router.get('/non-secret', ArticleController.getNonSecretArticles)
+router.get('/secret',
+  jwt({ secret, algorithms: ['HS256']}),
+  function(req, res) {
+    if (req.user.userType === 1 || req.user.userType === 0){
+      return ArticleController.getAllArticles(req, res)
+    } else {
+      return res.sendStatus(401)
+    }
+  })
 
 module.exports = router
