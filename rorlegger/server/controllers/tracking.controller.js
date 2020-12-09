6d4c041d47499d12390793ac7d03da857cd385d9
Trond Graabræk <43748103+trondag@users.noreply.github.com>
2020-12-09 16:51:40 +0100

    done

--- rorlegger/server/controllers/tracking.controller.js
+++ rorlegger/server/controllers/tracking.controller.js
@@ -19,3 +19,23 @@ exports.getViewsForAllArticles = async (req, res, next) => {
     return res.status(400).json({message: e.message})
   }
 }
+
+exports.getTopTenArticles = async (req, res, next) => {
+  try {
+    let views = await TrackingService.getTopTenArticles()
+    return res.status(200).json({message: "Successfully counted view", views})
+  } catch (e) {
+    console.error(e)
+    return res.status(400).json({message: e.message})
+  }
+}
+
+exports.getViewsPerUser = async (req, res, next) => {
+  try {
+    let views = await TrackingService.getViewsPerUser()
+    return res.status(200).json({message: "Successfully counted views", views})
+  } catch (e) {
+    console.error(e)
+    return res.status(400).json({message: e.message})
+  }
+}
